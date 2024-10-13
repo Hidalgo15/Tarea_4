4c846fac8021562ef80e0a43ed2db4550a2bc3b9
Emmanuel <emmanuelperezramirez15@gmail.com>
2024-10-13 00:34:28 -0400

    Actualizacion

--- obj/Debug/net8.0/Tarea_4.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tarea_4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f30cd33d41923810642c0ce66511e01e405a79f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
